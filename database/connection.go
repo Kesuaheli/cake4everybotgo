7ad52750f4447c28f1f202c3ad051a15bd5e8b80
Kesuaheli <40177436+Kesuaheli@users.noreply.github.com>
2023-03-19 00:38:42 +0100

    added database query functions

--- database/connection.go
+++ database/connection.go
@@ -33,6 +33,10 @@ type connection_config struct {
 	Database string `mapstructure:"database"`
 }
 
+func (c connection_config) String() string {
+	return fmt.Sprintf("user='%s', host:port='%s:%d', database='%s'", c.User, c.Host, c.Port, c.Database)
+}
+
 func Connect() {
 	log.Println("Connecting to Database...")
 
@@ -61,11 +65,41 @@ func Connect() {
 	log.Printf("Connected to database %s@%s:%d/%s\n", config.User, config.Host, config.Port, config.Database)
 }
 
+// Close closes the database and prevents new queries from starting.
+// Close then waits for all queries that have started processing on the server
+// to finish.
+//
+// It is rare to Close a DB, as the DB handle is meant to be
+// long-lived and shared between many goroutines.
 func Close() {
 	db.Close()
 	log.Println("Closed connection to database")
 }
 
-func (c connection_config) String() string {
-	return fmt.Sprintf("user='%s', host:port='%s:%d', database='%s'", c.User, c.Host, c.Port, c.Database)
+// Ping verifies a connection to the database is still alive,
+// establishing a connection if necessary.
+func Ping() (err error) {
+	return db.Ping()
+}
+
+// Exec executes a query without returning any rows.
+// The args are for any placeholder parameters in the query.
+func Exec(query string, args ...any) (result sql.Result, err error) {
+	return db.Exec(query, args...)
+}
+
+// Query executes a query that returns rows, typically a SELECT.
+// The args are for any placeholder parameters in the query.
+func Query(query string, args ...any) (rows *sql.Rows, err error) {
+	return db.Query(query, args...)
+}
+
+// QueryRow executes a query that is expected to return at most one row.
+// QueryRow always returns a non-nil value. Errors are deferred until
+// Row's Scan method is called.
+// If the query selects no rows, the *Row's Scan will return ErrNoRows.
+// Otherwise, the *Row's Scan scans the first selected row and discards
+// the rest.
+func QueryRow(query string, args ...any) (row *sql.Row) {
+	return db.QueryRow(query, args...)
 }
