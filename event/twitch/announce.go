6128945e071bdce1bd9e0037dbaf1c3a3c66bdb2
Kesuaheli <kesuaheligame@gmail.com>
2024-04-01 22:24:18 +0200

    updated twitch announcement
    added a few event type structs

--- event/twitch/announce.go
+++ event/twitch/announce.go
@@ -1,11 +1,39 @@
 package twitch
 
 import (
-	"cake4everybot/webserver/twitch"
+	webTwitch "cake4everybot/webserver/twitch"
+	"encoding/json"
 
 	"github.com/bwmarrin/discordgo"
 )
 
-func Announce(s *discordgo.Session, e *twitch.RawEvent) {
+func Announce(s *discordgo.Session, e *webTwitch.RawEvent) {
 	log.Printf("New twitch event:\n%+v\n", e)
+	data, _ := json.Marshal(e.Event)
+	switch e.Subscription.Type {
+	case "channel.update":
+		var e2 webTwitch.ChannelUpdateEvent
+		err := json.Unmarshal(data, &e2)
+		if err != nil {
+			log.Printf("failed to parse channel update event: %v", err)
+			return
+		}
+		log.Printf("Channel were updated: %v", e2)
+	case "stream.online":
+		var e2 webTwitch.StreamOnlineEvent
+		err := json.Unmarshal(data, &e2)
+		if err != nil {
+			log.Printf("failed to parse online event: %v", err)
+			return
+		}
+		log.Printf("Stream went online: %v", e2)
+	case "stream.offline":
+		var e2 webTwitch.StreamOfflineEvent
+		err := json.Unmarshal(data, &e2)
+		if err != nil {
+			log.Printf("failed to parse offline event: %v", err)
+			return
+		}
+		log.Printf("Stream went offline: %v", e2)
+	}
 }
--- webserver/twitch/global.go
+++ webserver/twitch/global.go
@@ -66,6 +66,32 @@ type SubscriptionTransport struct {
 	WebSocketSessionID string `json:"session_id,omitempty"`
 }
 
+type ChannelUpdateEvent struct {
+	BroadcasterUserID           string   `json:"broadcaster_user_id"`           // The broadcaster’s user ID.
+	BroadcasterUserLogin        string   `json:"broadcaster_user_login"`        // The broadcaster’s user login.
+	BroadcasterUserName         string   `json:"broadcaster_user_name"`         // The broadcaster’s user display name.
+	Title                       string   `json:"title"`                         // The channel’s stream title.
+	Language                    string   `json:"language"`                      // The channel’s broadcast language.
+	CategoryID                  string   `json:"category_id"`                   // The channel’s category ID.
+	CategoryName                string   `json:"category_name"`                 // The category name.
+	ContentClassificationLabels []string `json:"content_classification_labels"` // Array of content classification label IDs currently applied on the Channel. To retrieve a list of all possible IDs, use the Get Content Classification Labels API endpoint.
+}
+
+type StreamOnlineEvent struct {
+	ID                   string    `json:"id"`                     // The id of the stream.
+	BroadcasterUserID    string    `json:"broadcaster_user_id"`    // The broadcaster’s user id.
+	BroadcasterUserLogin string    `json:"broadcaster_user_login"` // The broadcaster’s user login.
+	BroadcasterUserName  string    `json:"broadcaster_user_name"`  // The broadcaster’s user display name.
+	Type                 string    `json:"type"`                   // The stream type. Valid values are: live, playlist, watch_party, premiere, rerun.
+	StartedAt            time.Time `json:"started_at"`             // The timestamp at which the stream went online at.
+}
+
+type StreamOfflineEvent struct {
+	BroadcasterUserID    string `json:"broadcaster_user_id"`    // The broadcaster’s user id.
+	BroadcasterUserLogin string `json:"broadcaster_user_login"` // The broadcaster’s user login.
+	BroadcasterUserName  string `json:"broadcaster_user_name"`  // The broadcaster’s user display name.
+}
+
 func RefreshSubscriptions() {
 	reqURL := "https://api.twitch.tv/helix/eventsub/subscriptions"
 	clientID := viper.GetString("twitch.clientID")
